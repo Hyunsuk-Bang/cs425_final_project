c66c675dd8604aacdc447042d6c92730ea8f6427
Hyunsuk-Bang <84240134+Hyunsuk-Bang@users.noreply.github.com>
2022-04-26 09:17:25 -0500

    initial commit

---
+++ product/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Manufacturer(models.Model):
+    manufacturer_id = models.CharField(primary_key=True, max_length=20)
+    manufacturer_name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.CharField(max_length=30)
+    phone_num = models.CharField(max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'manufacturer'
+
+class Product(models.Model):
+    p_id = models.CharField(primary_key=True, max_length=10)
+    category = models.CharField(max_length=30)
+    p_name = models.CharField(max_length=50)
+    wholesale_price = models.FloatField()
+    instore_price = models.FloatField()
+    manufacturer = models.ForeignKey(Manufacturer, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'product'
+
+class Shippingcompany(models.Model):
+    sc_id = models.CharField(primary_key=True, max_length=20)
+    sc_name = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'ShippingCompany'
+
---
+++ user/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+# Create your models here.
+class Member(models.Model):
+    m_id = models.CharField(primary_key=True, max_length=20)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(unique=True, max_length=20, blank=True, null=True)
+    type = models.IntegerField(blank=True, null=True)
+    user_status = models.IntegerField(blank=True, null=True)
+    reg_date = models.DateField()
+    billing_date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'member'
+
+class Bank(models.Model):
+    card_num = models.ForeignKey('Membercardinfo', models.DO_NOTHING, db_column='card_num', blank=True, null=True)
+    balance = models.FloatField()
+
+    class Meta:
+        managed = False
+        db_table = 'bank'
+
+class Memberaddress(models.Model):
+    m = models.ForeignKey(Member, models.DO_NOTHING, blank=True, null=True)
+    address1 = models.CharField(max_length=50)
+    address2 = models.CharField(max_length=50, blank=True, null=True)
+    state = models.CharField(max_length=2)
+    zipcode = models.CharField(max_length=15)
+
+    class Meta:
+        managed = False
+        db_table = 'memberAddress'
+
+class Membercardinfo(models.Model):
+    m = models.OneToOneField(Member, models.DO_NOTHING, primary_key=True)
+    card_num = models.CharField(unique=True, max_length=20)
+    card_name = models.CharField(max_length=30)
+    card_exp_month = models.IntegerField(blank=True, null=True)
+    card_exp_year = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'memberCardInfo'
+        unique_together = (('m', 'card_num'),)
+
